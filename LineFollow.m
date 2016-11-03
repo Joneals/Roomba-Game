0c2ca8c055948d9533cf2d59194a60ee6d4e352b
Jacob Reynolds <joreynolds97@gmail.com>
2016-11-03 16:42:52 -0400

    Final working version

--- LineFollow.m
+++ LineFollow.m
@@ -2,29 +2,29 @@
 function LineFollow(r)
     v = .1;
     offsets = [0 0];
-    thres = 2500;
+    thres = 2000;
     while 1
         %pause(.05)
         cliffs = r.getCliffSensors();
         %disp(cliffs)
         if (cliffs.leftFront < thres)
            offsets(1) = 0.2;
-           offsets(2) = 0;
+           offsets(2) = -0.07;
           % disp('Detected left!')
         
         elseif (cliffs.rightFront < thres)
            offsets(2) = 0.2;
-           offsets(1) = 0;
+           offsets(1) = -.07;
            % disp('Detected right!')
         
         
-        elseif (cliffs.left < thres)
-            offsets(1) = 0.25;
-           offsets(2) = -.2;
-        
-        elseif (cliffs.right < thres)
-            offsets(2) = 0.25;
-           offsets(1) = -.2;
+%         elseif (cliffs.left < thres)
+%             offsets(1) = 0.25;
+%            offsets(2) = -.2;
+%         
+%         elseif (cliffs.right < thres)
+%             offsets(2) = 0.25;
+%            offsets(1) = -.2;
         
         else
             offsets = [0 0];
