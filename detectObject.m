6859869b9ee84d079a65ca74d49d76fd943956f7
Jacob Reynolds <joreynolds97@gmail.com>
2016-11-05 15:27:44 -0400

    Added green and blue detection

--- detectObject.m
+++ detectObject.m
@@ -4,22 +4,22 @@ function detectObject(cam)
 hold on
  i = 0;
     while 1
-        
         img = cam.snapshot;
-        diff_im = imsubtract(img(:,:,3),rgb2gray(img));
-        diff_im = medfilt2(diff_im, [9 9]);
-        diff_im = im2bw(diff_im,.2);
-        diff_im = bwareaopen(diff_im, 500);
-        bw = bwlabel(diff_im, 8);
-        imshow(diff_im);
-        %stats.BoundingBox = [0 0 0 0];
-        
-        stats = regionprops(diff_im, 'BoundingBox', 'Centroid');
-        hold on
-        for obj=1:length(stats)
-            %rectangle('Position', stats(1).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
-            plot(stats(obj).Centroid(1), stats(obj).Centroid(2), 'r*');
+        imshow(img);
+        for i=1:3
+            diff_im = imsubtract(img(:,:,i),rgb2gray(img));
+            diff_im = medfilt2(diff_im, [8,8]);
+            diff_im = im2bw(diff_im, .2);
+            diff_im = bwareaopen(diff_im, 1500);
+            bw = bwlabel(diff_im, 8);
+            stats = regionprops(diff_im, 'BoundingBox', 'Centroid');
+            hold on
+            for obj=1:length(stats)
+                rectangle('Position', stats(obj).BoundingBox, 'EdgeColor', [1*i==1, 1*i==2, 1*i==3], 'LineWidth', 3);
+                plot(stats(obj).Centroid(1), stats(obj).Centroid(2),'o', 'MarkerFaceColor',[1*i==1, 1*i==2, 1*i==3] );
+            end
         end
+        
         drawnow
         hold off
         
