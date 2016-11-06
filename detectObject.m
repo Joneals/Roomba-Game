45addaef953f578258fe563a85fcb59095afc61a
Jacob Reynolds <joreynolds97@gmail.com>
2016-11-06 11:09:16 -0500

    Optimizations

--- detectObject.m
+++ detectObject.m
@@ -2,17 +2,18 @@ function detectObject(cam)
 % Initialization
 
 hold on
- i = 0;
     while 1
         img = cam.snapshot;
         imshow(img);
+        imgray = rgb2gray(img);
         for i=1:3
-            diff_im = imsubtract(img(:,:,i),rgb2gray(img));
+            diff_im = imsubtract(img(:,:,i),imgray);
             diff_im = medfilt2(diff_im, [8,8]);
             diff_im = im2bw(diff_im, .2);
             diff_im = bwareaopen(diff_im, 1500);
+            % To do - test if labeling is actually necessary
             bw = bwlabel(diff_im, 8);
-            stats = regionprops(diff_im, 'BoundingBox', 'Centroid');
+            stats = regionprops(bw, 'BoundingBox', 'Centroid');
             hold on
             for obj=1:length(stats)
                 rectangle('Position', stats(obj).BoundingBox, 'EdgeColor', [1*i==1, 1*i==2, 1*i==3], 'LineWidth', 3);
